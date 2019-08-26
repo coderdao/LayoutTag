a2cd0b36e8ea7ceab26461e20e6b7aeb1c0090c0
abo_o <abo2013@foxmail.com>
2019-08-26 22:18:27 +0800

    buc

--- src/TagsServiceProvider.php
+++ src/TagsServiceProvider.php
@@ -8,7 +8,7 @@ class TagsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $timestamp = date( 'Y_m_d_His');
+        $timestamp = date( 'Y_m_d_His' );
         $this->publishes( [
                 __DIR__ . '/V0/database/migrations/create_tags_tables.php'
                     => database_path("migrations/{$timestamp}_create_tags_tables.php"),
