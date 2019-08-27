1c00fd6b350a92df64aaefaf9e966e4b4b73cba1
abo_o <abo2013@foxmail.com>
2019-08-27 17:32:04 +0800

    buc

--- src/V0/Repository/LayoutTagRepository.php
+++ src/V0/Repository/LayoutTagRepository.php
@@ -2,6 +2,7 @@
 namespace Abo\LayoutTag\V0\Repository;
 
 use Abo\Generalutil\V1\Repositories\BaseRepository;
+use Abo\Generalutil\V1\Utils\AnalysisUtil;
 use Abo\LayoutTag\V0\Model\LayoutTag;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,6 +15,9 @@ class LayoutTagRepository extends BaseRepository
 
     /**
      * 自动添加 单个/一组 标签
+     * @param string|array $tag 标签 ( 单个|一组 )
+     * @return array [ '标签名' => 对应ids ]
+     * @throws \Exception
      */
     public function insertTags( $tag )
     {
@@ -24,10 +28,7 @@ class LayoutTagRepository extends BaseRepository
             $tagInfo = $this->insertTag( $tag ); // list( $tagName, $tagId ) = $this->insertTag( $tag );
             $ret2Return[ $tagInfo[0] ] = $tagInfo[1];
         }elseif ( is_array( $tag ) ) {
-            foreach ( $tag as $v2Tag ) {
-                $tagInfo = $this->insertTag( $tag ); // list( $tagName, $tagId ) = $this->insertTag( $tag );
-                $ret2Return[ $tagInfo[0] ] = $tagInfo[1];
-            }
+            $ret2Return = $this->insertTagArray( $tag );
         }
 
         return $ret2Return;
@@ -35,7 +36,7 @@ class LayoutTagRepository extends BaseRepository
 
     /**
      * 添加单个 标签
-     * @param string $tagName 标签名
+     * @param string $tagName 标签名( 单个 )
      * @return int|mixed
      * @throws \Exception
      */
@@ -58,5 +59,32 @@ class LayoutTagRepository extends BaseRepository
         }
 
         return [ $tagName, $ret2ExistTagId ];
+
+    }
+
+    /**
+     * 添加标签数组
+     * @param array $tagName 标签名 ( 数组 )
+     * @return array
+     * @throws \Exception
+     */
+    private function insertTagArray( array $tagName )
+    {
+        $tagName = array_unique( array_filter( $tagName ) );
+        if ( !$tagName ) { throw new \Exception( '标签名必填', -100 ); }
+
+        // 重复的 就直接取id ~ 1ms 左右,看数据库波动
+        $ret2ExistTagId = $this->Model->whereIn( 'name', $tagName )->pluck( 'id', 'name' );
+        if ( $ret2ExistTagId ){
+            $ret2Return = $ret2ExistTagId->toArray();
+            $tagName = array_diff( $tagName, array_keys( $ret2Return ) ); // 不存在标签留下一步新增
+        }
+
+        foreach ( $tagName as $v2Tag ) {
+            $tagInfo = $this->insertTag( $v2Tag ); // list( $tagName, $tagId ) = $this->insertTag( $tag );
+            $ret2Return[ $tagInfo[0] ] = $tagInfo[1];
+        }
+
+        return $ret2Return;
     }
 }
