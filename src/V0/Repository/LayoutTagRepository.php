d57588beed3a116f9e2b6fcd6636e8710ce5a419
abo_o <abo2013@foxmail.com>
2019-08-27 14:38:49 +0800

    buc

--- src/V0/Repository/LayoutTagRepository.php
+++ src/V0/Repository/LayoutTagRepository.php
@@ -12,10 +12,51 @@ class LayoutTagRepository extends BaseRepository
         parent::__construct( new LayoutTag() );
     }
 
-    public function insertTag( string $tagName = '' )
+    /**
+     * 自动添加 单个/一组 标签
+     */
+    public function insertTags( $tag )
     {
+        $ret2Return = [];
+        if( !$tag ){ return $ret2Return; }
 
+        if ( is_string( $tag ) ) {
+            $tagInfo = $this->insertTag( $tag ); // list( $tagName, $tagId ) = $this->insertTag( $tag );
+            $ret2Return[ $tagInfo[0] ] = $tagInfo[1];
+        }elseif ( is_array( $tag ) ) {
+            foreach ( $tag as $v2Tag ) {
+                $tagInfo = $this->insertTag( $tag ); // list( $tagName, $tagId ) = $this->insertTag( $tag );
+                $ret2Return[ $tagInfo[0] ] = $tagInfo[1];
+            }
+        }
 
-        self::duplicateKeyInsert( $this->Model->getTable() );
+        return $ret2Return;
+    }
+
+    /**
+     * 添加单个 标签
+     * @param string $tagName 标签名
+     * @return int|mixed
+     * @throws \Exception
+     */
+    private function insertTag( string $tagName = '' )
+    {
+        $tagName = trim( $tagName );
+        if ( !$tagName ) { throw new \Exception( '标签名必填', -100 ); }
+
+        /**
+         *self::duplicateKeyInsert( [ 'name' => trim( $tagName ) ], $this->Model->getTable() ); // 需要测试 返回更新 / 插入id
+         */
+
+        // ( 是否异常在标签 / 无则添加 ) 返回 id
+        /** @var $this->Model \Illuminate\Database\Query\Builder */
+        $ret2ExistTagId = $this->Model->where( 'name', $tagName )->value('id' );
+
+        if ( !$ret2ExistTagId ) {
+            $data2Insert = [ 'name' => trim( $tagName ) ];
+            $ret2ExistTagId = $this->Model->insertGetId( $data2Insert );
+        }
+
+        return [ $tagName, $ret2ExistTagId ];
     }
 }
