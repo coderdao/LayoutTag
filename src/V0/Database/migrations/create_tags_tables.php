c329488e2bac9483b9b5f48419638ee374cbda00
abo_o <abo2013@foxmail.com>
2019-08-28 10:47:22 +0800

    update

--- src/V0/Database/migrations/create_tags_tables.php
+++ src/V0/Database/migrations/create_tags_tables.php
@@ -17,7 +17,7 @@ class CreateTagsTables extends Migration
             Schema::create('layout_tag_index', function (Blueprint $table) {
                 $table->increments('id')->unsigned();
                 $table->string('name',64)->default( '' )->unique()->comment('标签名');
-                $table->string('attr',64)->default( '' )->unique()->comment('标签属性 [ 冗余字段 ]');
+                $table->string('attr',64)->default( '' )->comment('标签属性 [ 冗余字段 ]');
                 $table->timestamp('created_at')->nullable()->useCurrent();
                 $table->timestamp('updated_at')->nullable()->useCurrent();
             });
@@ -31,9 +31,11 @@ class CreateTagsTables extends Migration
                 $table->string( 'key_type', 30 )->comment('关联类型,确认对应表');
                 $table->unsignedInteger('key_id')->comment('关联id 与key_type配合使用');
                 $table->unsignedInteger('tag_id')->comment('layout_tag_index 主键 id');
-                $table->string('name',64)->default( '' )->unique()->comment('标签名[ 冗余 ]');
-                $table->string('attr',64)->default( '' )->unique()->comment('标签属性 [ 冗余字段 ]');
+                $table->string('name',64)->default( '' )->comment('标签名[ 冗余 ]');
+                $table->string('attr',64)->default( '' )->comment('标签属性 [ 冗余字段 ]');
                 $table->timestamp('created_at')->nullable()->useCurrent();
+
+
             });
         }else{
             echo 'layout_tag_relation 表已存在'."\r\n";
