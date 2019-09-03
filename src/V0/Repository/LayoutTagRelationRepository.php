5df374d50efd2f4ddfdac1ad9f9fbe16b6db9c5f
abo_o <abo2013@foxmail.com>
2019-09-03 14:01:13 +0800

    update

--- src/V0/Repository/LayoutTagRelationRepository.php
+++ src/V0/Repository/LayoutTagRelationRepository.php
@@ -41,6 +41,20 @@ class LayoutTagRelationRepository extends BaseRepository
         return $SearchModel->toArray();
     }
 
+    public function saveTagRelation( string $keyType, $keyId, $tagName )
+    {
+        $ret2Return = '';
+        if ( is_int( $keyId ) ) {
+            $ret2Return = $this->saveTagRelationOne( $keyType, $keyId, $tagName );
+        }elseif( strstr( $keyId, ',' ) || is_array( $keyId ) ){
+            if ( strstr( $keyId, ',' ) ) { $keyId = explode( ',', $keyId ); }
+
+            foreach ( $keyId as $v2KeyId ) {
+                $ret2Return = $this->saveTagRelationOne( $keyType, $v2KeyId, $tagName );
+            }
+        }
+    }
+
     /**
      * 保存 ( 增/改 ) 标签信息
      * @method LayoutTagRelationRepository::saveTagRelation
@@ -50,7 +64,7 @@ class LayoutTagRelationRepository extends BaseRepository
      * @return bool|int|mixed 保存个数
      * @throws \Exception
      */
-    public function saveTagRelation( string $keyType, int $keyId, $tagName )
+    private function saveTagRelationOne( string $keyType, $keyId, $tagName )
     {
         if ( !$keyType || !$keyId || !$tagName ) {
             return false;
@@ -61,19 +75,47 @@ class LayoutTagRelationRepository extends BaseRepository
         if ( !$tagArray ) { throw new \Exception( '未成功添加标签', -100 ); }
 
         // 保存标签关系
-        $i2Count = 0;
         foreach ( $tagArray as $k2TagName => $v2TagId ) {
             if ( !$k2TagName || !$v2TagId ) { continue; }
-            $data2Insert = [
+            $data2Insert[] = [
                 'key_type' => $keyType,
                 'key_id' => $keyId,
                 'tag_id' => $v2TagId,
                 'name' => $k2TagName,
             ];
+        }
+        $ret = self::duplicateKeyInsertArray( $data2Insert, $this->Model->getTable() );
 
-            $i2Count += self::duplicateKeyInsert( $data2Insert, $this->Model->getTable() );
+        return $ret;
+    }
+
+    /**
+     * 删除标签关联
+     * @method LayoutTagRelationRepository::delTagRelation
+     * @param string $keyType 关联类型
+     * @param int $keyId 关联id
+     * @param string|array $tagName 标签
+     * @return bool|int|null
+     */
+    public function delTagRelation( string $keyType, int $keyId, $tagName )
+    {
+        if ( !$keyType || !$keyId ) {
+            return false;
         }
 
-        return $i2Count;
+        $SearchModel = $this->Model->where( 'key_type', '=', trim( $keyType ) )
+            ->where( 'key_id', '=', intval( $keyId ) );
+
+        if ( is_string( $tagName ) ) {
+            $SearchModel = $SearchModel->where( 'name', '=', trim( $tagName ) );
+        }elseif( is_array( $tagName ) ) {
+            foreach ( $tagName as &$v2Tag ) {
+                $v2Tag = trim( $v2Tag );
+            }
+
+            $SearchModel = $SearchModel->whereIn( 'name', $tagName );
+        }
+
+        return $SearchModel->delete();
     }
 }
