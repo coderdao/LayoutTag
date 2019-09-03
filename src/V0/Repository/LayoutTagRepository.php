6023eff94f9124132b235f909dc6b36eda30bfc6
abo_o <abo2013@foxmail.com>
2019-09-03 10:36:11 +0800

    update

--- src/V0/Repository/LayoutTagRepository.php
+++ src/V0/Repository/LayoutTagRepository.php
@@ -34,6 +34,30 @@ class LayoutTagRepository extends BaseRepository
         return $ret2Return;
     }
 
+    private function insertTagArrayV2( array $tagName )
+    {
+        $tagName = array_unique( array_filter( $tagName ) );
+        if ( !$tagName ) { throw new \Exception( '标签名必填', -100 ); }
+
+        // 重复的 就直接取id ~ 1ms 左右,看数据库波动
+        $ret2ExistTagId = $this->Model->whereIn( 'name', $tagName )->pluck( 'id', 'name' );
+        if ( $ret2ExistTagId ){
+            $ret2Return = $ret2ExistTagId->toArray();
+            $tagName = array_diff( $tagName, array_keys( $ret2Return ) ); // 不存在标签留下一步新增
+        }
+
+        // 组织批量插入数组
+        foreach ( $tagName as $v2Tag ) {
+            $data2Insert[] = [ 'name' => trim( $v2Tag ) ];
+        }
+        $ret2ExistTagId = $this->Model->insert( $data2Insert );
+
+        // 获取全部 tag & id
+        $ret2Return = $this->Model->whereIn( 'name', $tagName )->pluck( 'id', 'name' )->toArray();
+
+        return $ret2Return;
+    }
+
     /**
      * 添加单个 标签
      * @param string $tagName 标签名( 单个 )
@@ -44,10 +68,6 @@ class LayoutTagRepository extends BaseRepository
     {
         $tagName = trim( $tagName );
         if ( !$tagName ) { throw new \Exception( '标签名必填', -100 ); }
-
-        /**
-         *self::duplicateKeyInsert( [ 'name' => trim( $tagName ) ], $this->Model->getTable() ); // 需要测试 返回更新 / 插入id
-         */
 
         // ( 是否异常在标签 / 无则添加 ) 返回 id
         /** @var $this->Model \Illuminate\Database\Query\Builder */
