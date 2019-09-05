e158020c99de02461fffaabca049fdf4ff810361
abo_o <abo2013@foxmail.com>
2019-09-05 21:23:49 +0800

    update

--- src/V1/Conts/LayoutTagTableConts.php
+++ src/V1/Conts/LayoutTagTableConts.php
@@ -13,5 +13,15 @@ class LayoutTagTableConts
 {
     const TABLE_NAME_FLAG = '#TABLE_NAME#';
 
-
+    const CREATE_TABLE_SQL = "CREATE TABLE `" . self::TABLE_NAME_FLAG . "_tag_index` (
+      `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+      `name` varchar(64) NOT NULL DEFAULT '' COMMENT '标签名',
+      `attr` varchar(64) NOT NULL DEFAULT '' COMMENT '标签属性 [ 冗余字段 ]',
+      `admin_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '添加人id',
+      `admin_name` varchar(64) NOT NULL DEFAULT '' COMMENT '添加人名称',
+      `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+      `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+      PRIMARY KEY (`id`),
+      UNIQUE KEY `idx_name` (`name`) USING BTREE
+    ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;";
 }
