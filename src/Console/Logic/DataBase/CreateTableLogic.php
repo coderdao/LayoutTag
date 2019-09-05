c9f47927753d940de65660f194f4df2737193ec1
abo_o <abo2013@foxmail.com>
2019-09-05 21:18:29 +0800

    update

---
+++ src/Console/Logic/DataBase/CreateTableLogic.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Function:
+ * Description:
+ * Abo 2019/9/5 21:18
+ * Email: abo2013@foxmail.com
+ */
+
+namespace Abo\LayoutTag\Console\Logic\DataBase;
+
+
+class CreateTableLogic
+{
+
+}
