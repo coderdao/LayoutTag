19bf320970232828ee702779680629678bf7e54a
abo_o <abo2013@foxmail.com>
2019-09-08 15:44:56 +0800

    update

---
+++ src/V1/Repository/LayoutTagRelationRepository.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Abo\LayoutTag\V0\Repository;
+
+
+use Abo\Generalutil\V1\Repositories\BaseRepository;
+use Abo\LayoutTag\V0\Model\LayoutTagRelation;
+use Illuminate\Database\Eloquent\Model;
+
+class LayoutTagRelationRepository extends BaseRepository
+{
+    public function __construct()
+    {
+        parent::__construct( new LayoutTagRelation() );
+    }
+
+    /**
+     * 某实体的关联标签列表
+     * @method LayoutTagRelationRepository::listRelationTags
+     * @param string $keyType 实体类型
+     * @param int $keyId      实体id
+     * @return array          关联标签列表
+     * @throws \Exception
+     */
+    public function listRelationTags( string $keyType, int $keyId )
+    {
+        // if ( !$keyType && ( !$keyId && !$tagName ) ) { throw new \Exception( '暂无更多信息', -100 ); }
+        if ( !$keyType || !$keyId ) { throw new \Exception( '暂无更多信息', -100 ); }
+
+        $SearchModel = $this->Model->select( [ 'tag_id', 'name' ] )
+            ->where( 'key_type', '=', $keyType );
+        if ( $keyId ) {
+            $SearchModel = $SearchModel->where( 'key_id', '=', $keyId );
+        }
+
+        $SearchModel = $SearchModel->get();
+        if ( !$SearchModel ) {
+            return [];
+        }
+
+        return $SearchModel->toArray();
+    }
+
+    public function saveTagRelation( string $keyType, $keyId, $tagName )
+    {
+        $ret2Return = false;
+        $keyId = explode( ',', trim( $keyId, ',' ) );
+        foreach ( $keyId as $v2KeyId ) {
+            $ret2Return = $this->saveTagRelationOne( $keyType, $v2KeyId, $tagName );
+        }
+
+        return $ret2Return;
+    }
+
+    /**
+     * 保存 ( 增/改 ) 标签信息
+     * @method LayoutTagRelationRepository::saveTagRelation
+     * @param string $keyType 关联类型
+     * @param int $keyId      关联id
+     * @param string $tagName        标签名
+     * @return bool|int|mixed 保存个数
+     * @throws \Exception
+     */
+    private function saveTagRelationOne( string $keyType, $keyId, $tagName )
+    {
+        if ( !$keyType || !$keyId || !$tagName ) {
+            return false;
+        }
+
+        // 寻找 / 添加标签
+        $tagArray = ( new LayoutTagRepository() )->findOrCreateTags( $tagName );
+        if ( !$tagArray ) { throw new \Exception( '未成功添加标签', -100 ); }
+
+        // 保存标签关系
+        foreach ( $tagArray as $k2TagName => $v2TagId ) {
+            if ( !$k2TagName || !$v2TagId ) { continue; }
+            $data2Insert[] = [
+                'key_type' => $keyType,
+                'key_id' => $keyId,
+                'tag_id' => $v2TagId,
+                'name' => $k2TagName,
+            ];
+        }
+        $ret = self::duplicateKeyInsertArray( $data2Insert, $this->Model->getTable() );
+
+        return $ret;
+    }
+
+    /**
+     * 删除标签关联
+     * @method LayoutTagRelationRepository::delTagRelation
+     * @param string $keyType 关联类型
+     * @param int $keyId 关联id
+     * @param string|array $tagName 标签
+     * @return bool|int|null
+     */
+    public function delTagRelation( string $keyType, int $keyId, $tagName )
+    {
+        if ( !$keyType || !$keyId ) {
+            return false;
+        }
+
+        $SearchModel = $this->Model->where( 'key_type', '=', trim( $keyType ) )
+            ->where( 'key_id', '=', intval( $keyId ) );
+
+        if ( is_string( $tagName ) ) {
+            $SearchModel = $SearchModel->where( 'name', '=', trim( $tagName ) );
+        }elseif( is_array( $tagName ) ) {
+            foreach ( $tagName as &$v2Tag ) {
+                $v2Tag = trim( $v2Tag );
+            }
+
+            $SearchModel = $SearchModel->whereIn( 'name', $tagName );
+        }
+
+        return $SearchModel->delete();
+    }
+}
---
+++ src/V1/Repository/LayoutTagRepository.php
@@ -0,0 +1,93 @@
+<?php
+namespace Abo\LayoutTag\V0\Repository;
+
+use Abo\Generalutil\V1\Repositories\BaseRepository;
+use Abo\Generalutil\V1\Utils\AnalysisUtil;
+use Abo\LayoutTag\V0\Model\LayoutTag;
+use Illuminate\Database\Eloquent\Model;
+
+class LayoutTagRepository extends BaseRepository
+{
+    public function __construct()
+    {
+        parent::__construct( new LayoutTag() );
+    }
+
+    /**
+     * 自动添加 单个/一组 标签
+     * @param string|array $tag 标签 ( 单个|一组 )
+     * @return array [ '标签名' => 对应ids ]
+     * @throws \Exception
+     */
+    public function findOrCreateTags( $tag )
+    {
+        $ret2Return = [];
+        if( !$tag ){ return $ret2Return; }
+
+        if ( is_string( $tag ) ) {
+            $tagInfo = $this->insertTag( $tag ); // list( $tagName, $tagId ) = $this->insertTag( $tag );
+            $ret2Return[ $tagInfo[0] ] = $tagInfo[1];
+        }elseif ( is_array( $tag ) ) {
+            $ret2Return = $this->insertTagArrayV2( $tag );
+        }
+
+        return $ret2Return;
+    }
+
+    /**
+     * 添加标签数组
+     * @method LayoutTagRepository::insertTagArrayV2
+     * @description [3次链接]查询去重, 插入,查询 ( 唯一键会保持不会重复,但会抛异常,这里忽略 )
+     * @param array $tagName 标签名 ( 数组 )
+     * @return array
+     * @throws \Exception
+     */
+    private function insertTagArrayV2( array $tagName )
+    {
+        $tagName = array_unique( array_filter( $tagName ) );
+        if ( !$tagName ) { throw new \Exception( '标签名必填', -100 ); }
+
+        // 重复的 就直接取id ~ 1ms 左右,看数据库波动
+        $ret2ExistTagId = $this->Model->whereIn( 'name', $tagName )->pluck( 'id', 'name' );
+        if ( $ret2ExistTagId ){
+            $ret2Return = $ret2ExistTagId->toArray();
+            $tagName = array_diff( $tagName, array_keys( $ret2Return ) ); // 不存在标签留下一步新增
+        }
+        if ( !$tagName ) { return $ret2Return; }
+
+        // 组织批量插入数组
+        foreach ( $tagName as $v2Tag ) {
+            $data2Insert[] = [ 'name' => trim( $v2Tag ) ];
+        }
+        $this->Model->insert( $data2Insert );
+
+        // 获取全部 tag & id
+        $ret2Return = $this->Model->whereIn( 'name', $tagName )->pluck( 'id', 'name' )->toArray();
+
+        return $ret2Return;
+    }
+
+    /**
+     * 添加单个 标签
+     * @param string $tagName 标签名( 单个 )
+     * @return int|mixed
+     * @throws \Exception
+     */
+    private function insertTag( string $tagName = '' )
+    {
+        $tagName = trim( $tagName );
+        if ( !$tagName ) { throw new \Exception( '标签名必填', -100 ); }
+
+        // ( 是否异常在标签 / 无则添加 ) 返回 id
+        /** @var $this->Model \Illuminate\Database\Query\Builder */
+        $ret2ExistTagId = $this->Model->where( 'name', $tagName )->value('id' );
+
+        if ( !$ret2ExistTagId ) {
+            $data2Insert = [ 'name' => trim( $tagName ) ];
+            $ret2ExistTagId = $this->Model->insertGetId( $data2Insert );
+        }
+
+        return [ $tagName, $ret2ExistTagId ];
+
+    }
+}
